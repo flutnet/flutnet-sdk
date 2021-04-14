ff6d28dc639426689cbca2c0c028491e9dc9c1ce
micneg <michele.negri@novagemsolutions.com>
2021-04-14 10:30:58 +0200

    - Changed license to LGPL v3 - Added header with copyright and license information to each .cs file

--- src/Flutnet.Cli.Core/Utilities/ReflectionHelper.cs
+++ src/Flutnet.Cli.Core/Utilities/ReflectionHelper.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿// Copyright (c) 2020-2021 Novagem Solutions S.r.l.
+//
+// This file is part of Flutnet.
+//
+// Flutnet is a free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Flutnet is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Flutnet.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
--- src/HtmlRenderer.Core/Core/Dom/CssBoxFrame.cs
+++ src/HtmlRenderer.Core/Core/Dom/CssBoxFrame.cs
@@ -1,14 +1,19 @@
-﻿// "Therefore those skilled at the unorthodox
-// are infinite as heaven and earth,
-// inexhaustible as the great rivers.
-// When they come to an end,
-// they begin again,
-// like the days and months;
-// they die and are reborn,
-// like the four seasons."
-// 
-// - Sun Tsu,
-// "The Art of War"
+﻿// Copyright (c) 2020-2021 Novagem Solutions S.r.l.
+//
+// This file is part of Flutnet.
+//
+// Flutnet is a free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Flutnet is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Flutnet.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using System.Net;
